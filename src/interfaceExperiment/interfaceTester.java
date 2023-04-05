231d5b3d6535af2f857477718970ba9bea2176dc
Hidayet Eren Uludağ <76475118+mightymountain@users.noreply.github.com>
2023-04-05 13:27:24 +0300

    test

--- src/interfaceExperiment/interfaceTester.java
+++ src/interfaceExperiment/interfaceTester.java
@@ -2,9 +2,9 @@ package interfaceExperiment;
 
 public class interfaceTester {
     public static void main(String[] args) {
-        circInterface cı = new circInterface();
-        System.out.println(cı.surfaceArea());
-        System.out.println(cı.area());
+        circInterface c1 = new circInterface();
+        System.out.println(c1.surfaceArea());
+        System.out.println(c1.area());
 
     }
 }
